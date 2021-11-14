413281302729a995809b023a221a3b83f4b787e1
sophi <ss@covizmo.com>
2021-11-14 20:26:13 +0200

    add MedHelper_UI: Welcome page, Sign in page, Sign up page

---
+++ MedHelper_UI/Page_signUp.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MedHelper_UI
+{
+    /// <summary>
+    /// Interaction logic for Page_signIn.xaml
+    /// </summary>
+    public partial class Page_signUp : Page
+    {
+        public Page_signUp()
+        {
+            InitializeComponent();
+        }
+
+        private void BtmClickRegister(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}

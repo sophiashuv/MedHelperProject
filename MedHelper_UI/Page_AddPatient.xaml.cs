bfac40cce78855cacdc82b40d5ac36d705749744
sophi <ss@covizmo.com>
2021-11-17 20:13:28 +0200

    add addPatient Page

---
+++ MedHelper_UI/Page_AddPatient.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MedHelper_UI
+{
+    /// <summary>
+    /// Interaction logic for Page_AddPatient.xaml
+    /// </summary>
+    public partial class Page_AddPatient : Page
+    {
+        public Page_AddPatient()
+        {
+            InitializeComponent();
+        }
+    }
+}

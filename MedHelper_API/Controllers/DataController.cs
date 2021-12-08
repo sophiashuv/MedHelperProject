db6d0bf9de5e1c38135884c566c0617f5f1afced
Pavlo Zabavskyy <pavlo.zabavskyi@lnu.edu.ua>
2021-12-08 12:17:58 +0200

    [ADD] added search by "pharmacotherapeuticGroup"

--- MedHelper_API/Controllers/DataController.cs
+++ MedHelper_API/Controllers/DataController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using MedHelper_API.Responses;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using MyApi.Controllers;
+using Newtonsoft.Json;
 
 namespace MedHelper_API.Controllers
 {
@@ -41,6 +43,69 @@ namespace MedHelper_API.Controllers
             var result = _mapper.Map<List<Disease>, List<DiseaseResponse>>(diseases);
 
             return Ok(result);
+        }
+        
+        [HttpGet("patient/{id}/search={search}")]
+        public async Task<ActionResult> Search(int id, string search)
+        {
+            var user = GetCurrentUserId();
+            // search = "а";
+            
+            var patient = await _context.Patients
+                .Include(obj => obj.PatientDiseases)
+                .FirstOrDefaultAsync(obj => obj.PatientID == id && obj.DoctorID == user);
+            
+            if (patient is null)
+            {
+                return NotFound();
+            }
+
+            var patientDiseases = await _context.Diseases.Where(obj =>
+                patient.PatientDiseases.Select(o => o.DiseaseID).Contains(obj.DiseaseID)).ToListAsync();
+
+            var medicines = await _context.Medicines
+                .Include(obj => obj.MedicineCompositions)
+                .Include(obj => obj.MedicineContraindications)
+                .Include(obj => obj.MedicineInteractions)
+                .Where(obj => obj.pharmacotherapeuticGroup.ToLower().Contains(search.ToLower()))
+                .ToListAsync();
+
+            var compositions = await _context.Compositions.ToListAsync();
+            var contraindications = await _context.Contraindications.ToListAsync();
+            var interactions = await _context.MedicineInteraction.Include(obj => obj.Composition).ToListAsync();
+
+            var responseData = from medicine in medicines
+                from composition in medicine.MedicineCompositions
+                join comp in compositions on composition.CompositionID equals comp.CompositionID
+                from contraindication in medicine.MedicineContraindications
+                join contr in contraindications on contraindication.ContraindicationID equals contr.ContraindicationID
+                from interaction in medicine.MedicineInteractions
+                join intr in interactions on interaction.MedicineInteractionID equals intr.MedicineInteractionID
+                where !patientDiseases.Select(obj => obj.Title).Contains(comp.Description)
+                select new
+                {
+                    medicine.MedicineID,
+                    medicine.Name,
+                    medicine.pharmacotherapeuticGroup,
+                    CompositionDescription = comp.Description,
+                    ContraindicationDescription = contr.Description,
+                    MedicineInteractionDescription = intr.Description
+                };
+
+            var response = from medicine in responseData
+                group medicine by new { medicine.MedicineID, medicine.Name, medicine.pharmacotherapeuticGroup }
+                into grouped
+                select new
+                {
+                    grouped.Key.MedicineID,
+                    grouped.Key.Name,
+                    grouped.Key.pharmacotherapeuticGroup,
+                    Compositions = grouped.Select(obj => obj.CompositionDescription).Distinct(),
+                    Contraindications = grouped.Select(obj => obj.ContraindicationDescription).Distinct(),
+                    MedicineInteractions = grouped.Select(obj => obj.MedicineInteractionDescription).Distinct()
+                };
+            
+            return Ok(response);
         }
     }
 }

6f2ea8812267242f4d6d1867e27c9d24043e0fd8
sophi <ss@covizmo.com>
2021-12-09 02:04:20 +0200

    add medicine description

--- MedHelper_UI/Page_PatientInfo.xaml.cs
+++ MedHelper_UI/Page_PatientInfo.xaml.cs
@@ -24,7 +24,19 @@ namespace MedHelper_UI
         public Page_Doctor MainWindow;
         public List<String> medicines = new List<string> { "Medicine1", "Medicine2", "Medicine3", "Medicine4", "Medicine5", "Medicine6", "Medicine7", "Medicine8", "Medicine9"};
         public List<Button> buttons;
-        public PatientInfo(Page_Doctor mainWindoe)
+        public StackPanel medicinePanel = new StackPanel();
+        private int UserId;
+
+        public static TextBlock CreateMedicineText(String text, string color, bool bold=true)
+        {
+            var medicineText = new TextBlock();
+            medicineText.Text = text;
+            medicineText.Margin = new Thickness(10, 0, 5, 0);
+            medicineText.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString(color));
+            if (bold) medicineText.FontWeight = FontWeights.Bold;
+            return medicineText;
+        }
+        public PatientInfo(Page_Doctor mainWindow, int userId)
         {
             MainWindow = mainWindoe;
             InitializeComponent();
@@ -50,26 +62,51 @@ namespace MedHelper_UI
             var button_num = 0;
             var medicinePanel = new StackPanel();
             medicinePanel.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffffff"));
-            var medicineText = new TextBlock();
-            var medicineDescription = "Не слід порушувати рекомендації щодо застосування лікарського засобу ‒ це може зашкодити здоров’ю. " +
-                "Не слід перевищувати рекомендовані дози лікарського засобу. " +
-                "При застосуванні препарату слід утримуватися від вживання алкоголю. " +
-                "Препарат містить етанол.Не рекомендується застосування лікарського засобу протягом тривалого часу." +
-                " Валеріана може виявляти помірний депресивний ефект, тому не рекомендується сумісний прийом лікарського " +
-                "засобу із синтетичними седативними засобами через потенціювання ефекту. " +
-                "При вираженому атеросклерозі мозкових судин препарат можна застосовувати тільки під контролем лікаря. " +
-                "У процесі зберігання допускається випадання осаду. Перед застосуванням препарат слід збовтувати. " +
-                "Валеріанка - Вішфа є традиційним лікарським засобом для використання відповідно до показань, підтверджених тривалим застосуванням. " +
-                "Застосування у період вагітності або годування груддю " +
-                "Препарат не застосовувати у період вагітності або годування груддю через вміст етанолу 70 %. " +
-                "Здатність впливати на швидкість реакції при керуванні автотранспортом або іншими механізмами. " +
-                "На період лікування препаратом слід утримуватися від керування транспортними засобами та роботи з потенційно ‒ небезпечними механізмами через вміст етанолу 70 % та можливість розвитку побічних ефектів з боку центральної нервової системи.";
+            var medicineName = CreateMedicineText("Medicine Name: ", "#FFED635E");
+            var medicineName2 = CreateMedicineText("Medicine ", "#000000", false);
+            var StackPName = new StackPanel();
+            StackPName.Orientation = Orientation.Horizontal;
+            StackPName.Children.Add(medicineName);
+            StackPName.Children.Add(medicineName2);
+            medicinePanel.Children.Add(StackPName);
 
-            medicineDescription = Regex.Replace(medicineDescription, ".{50}", "$0\n");
-            medicineText.Text =  medicineDescription;
-            medicineText.Margin = new Thickness(10, 0, 5, 0);
-            medicineText.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFED635E"));
-            medicinePanel.Children.Add(medicineText);
+            var medicineGroup = CreateMedicineText("Medicine Group: ", "#FFED635E");
+            var medicineGroup2 = CreateMedicineText("Group ", "#000000", false);
+            var StackPGroup = new StackPanel();
+            StackPGroup.Orientation = Orientation.Horizontal;
+            StackPGroup.Children.Add(medicineGroup);
+            StackPGroup.Children.Add(medicineGroup2);
+            medicinePanel.Children.Add(StackPGroup);
+
+            var composition = new List<String> { "Composition1", "Composition2", "Composition3" };
+            var compositionString = Regex.Replace(String.Join(", ", composition), ".{27}", "$0\n");
+            var medicineComposition = CreateMedicineText("Medicine Composition: ", "#FFED635E");
+            var medicineComposition2 = CreateMedicineText(compositionString, "#000000", false);
+            var StackPComposition = new StackPanel();
+            StackPComposition.Orientation = Orientation.Horizontal;
+            StackPComposition.Children.Add(medicineComposition);
+            StackPComposition.Children.Add(medicineComposition2);
+            medicinePanel.Children.Add(StackPComposition);
+
+            var interaction = new List<String> { "Interaction1", "Interaction2", "Interaction3" };
+            var interactionString = Regex.Replace(String.Join(", ", interaction), ".{33}", "$0\n");
+            var medicineInteraction = CreateMedicineText("Medicine Interaction: ", "#FFED635E");
+            var medicineInteraction2 = CreateMedicineText(interactionString, "#000000", false);
+            var StackPInteraction = new StackPanel();
+            StackPInteraction.Orientation = Orientation.Horizontal;
+            StackPInteraction.Children.Add(medicineInteraction);
+            StackPInteraction.Children.Add(medicineInteraction2);
+            medicinePanel.Children.Add(StackPInteraction);
+
+            var сontraindication = new List<String> { "Contraindication1", "Contraindication2", "Contraindication3" };
+            var сontraindicationString = Regex.Replace(String.Join(", ", сontraindication), ".{25}", "$0\n");
+            var medicineContraindication = CreateMedicineText("Medicine Contraindication: ", "#FFED635E");
+            var medicineContraindication2 = CreateMedicineText(сontraindicationString, "#000000", false);
+            var StackPContraindication = new StackPanel();
+            StackPContraindication.Orientation = Orientation.Horizontal;
+            StackPContraindication.Children.Add(medicineContraindication);
+            StackPContraindication.Children.Add(medicineContraindication2);
+            medicinePanel.Children.Add(StackPContraindication);
 
             for (int i = 0; i < button_num + 1; i++)
             {
